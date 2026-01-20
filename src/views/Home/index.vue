d1fca884d1f17edd4c158253915975a76f0f92bf
GoshenC <675466480@qq.com>
2026-01-20 22:05:47 +0800

    上传本地代码

---
+++ src/views/Home/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from 'vue';
+import {ref} from 'vue'
+import HomeBanner from './components/HomeBanner.vue';
+import HomeCategory from './components/HomeCategory.vue';
+import HomeNew from './components/HomeNew.vue';
+import HomeHot from './components/HomeHot.vue';
+import HomeProduct from './components/HomeProduct.vue';
+import HomePanel from './components/HomePanel.vue';
+
+
+</script>
+
+<template>
+  <div class="container">
+    <HomeCategory/>
+    <HomeBanner/>
+</div>
+<HomeHot/>
+<HomeNew/>
+
+<HomeProduct/>
+
+</template>
+
+<style scoped>
+/* 样式写这里 */
+</style>
